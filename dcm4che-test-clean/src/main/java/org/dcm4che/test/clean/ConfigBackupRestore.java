6823922e64173e027f3b335324f7a36543344222
Hermann Czedik-Eysenberg <hermann.czedik-eysenberg@agfa.com>
2015-06-01 18:18:40 +0200

    [DCMTEST-25] Configuration should be set to default before every test

--- dcm4che-test-clean/src/main/java/org/dcm4che/test/clean/ConfigBackupRestore.java
+++ dcm4che-test-clean/src/main/java/org/dcm4che/test/clean/ConfigBackupRestore.java
@@ -1,18 +1,24 @@
 package org.dcm4che.test.clean;
 
-import org.dcm4che3.conf.api.internal.DicomConfigurationManager;
-import org.dcm4che3.conf.core.api.Configuration;
-import org.dcm4che3.conf.core.api.ConfigurationException;
-import org.dcm4che3.conf.dicom.DicomConfigurationBuilder;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import java.util.Map;
+
+import org.dcm4che3.conf.api.internal.DicomConfigurationManager;
+import org.dcm4che3.conf.core.api.Configuration;
+import org.dcm4che3.conf.core.api.ConfigurationException;
+import org.dcm4che3.conf.dicom.CommonDicomConfiguration.DicomConfigurationRootNode;
+import org.dcm4che3.conf.dicom.DicomConfigurationBuilder;
 
 /**
+ * Web service which allows to restore the complete Configuration to a default
+ * state.
+ * 
  * @author Roman K
+ * @author Hermann Czedik-Eysenberg <hermann-agfa@czedik.net>
  */
 @ApplicationScoped
 @Path("/config-restore")
@@ -28,14 +34,14 @@ public class ConfigBackupRestore {
         configurationManager = DicomConfigurationBuilder.newConfigurationBuilder(System.getProperties()).build();
 
         Configuration configurationStorage = configurationManager.getConfigurationStorage();
-        defaultConfig = (Map<String, Object>) configurationStorage.getConfigurationNode("/", null);
+        defaultConfig = (Map<String, Object>) configurationStorage.getConfigurationNode("/", DicomConfigurationRootNode.class);
     }
 
 
     @GET
     @Path("/")
     public void restoreConfig() throws ConfigurationException {
-        configurationManager.getConfigurationStorage().persistNode("/", defaultConfig, null);
+        configurationManager.getConfigurationStorage().persistNode("/", defaultConfig, DicomConfigurationRootNode.class);
     }
 
 }
