74cab7a12ffdd010e0c4f453c64ea11629cc6539
Hermann Czedik-Eysenberg <hermann.czedik-eysenberg@agfa.com>
2015-06-22 17:03:54 +0200

    [DCMTEST-30] pixel comparison
    Provide utility methods for checking DICOM objects

---
+++ dcm4che-test/src/main/java/org/dcm4che/test/data/DicomAssert.java
@@ -0,0 +1,186 @@
+//
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Agfa Healthcare.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.dcm4che.test.data;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.ElementDictionary;
+import org.junit.Assert;
+
+/**
+ * Assertions on DICOM objects for tests.
+ * 
+ * @author Hermann Czedik-Eysenberg <hermann-agfa@czedik.net>
+ */
+public class DicomAssert {
+
+    /**
+     * Check that the content of the given DICOM dataset is equal to the given
+     * reference dataset.
+     * 
+     * @param dataset
+     *            dataset
+     * @param referenceDataset
+     *            reference dataset
+     */
+    public static void assertEquals(Attributes dataset, Attributes referenceDataset)
+    {
+        boolean equal = dataset.equals(referenceDataset);
+
+        if (!equal)
+        {
+            // TODO output information about failure
+
+            Assert.fail("The dicom objects are not equal");
+        }
+    }
+
+    /**
+     * Check that the content of the given DICOM file is equal to the given
+     * reference DICOM file.
+     * 
+     * File meta information is NOT considered.
+     * 
+     * @param dicomFile
+     *            DICOM file
+     * @param dicomReferenceFile
+     *            reference DICOM file
+     * @throws IOException
+     */
+    public static void assertEqual(Path dicomFile, Path dicomReferenceFile) throws IOException
+    {
+        assertEquals(DicomUtils.read(dicomFile), DicomUtils.read(dicomReferenceFile));
+    }
+
+    /**
+     * Check that dataset contains the given tag and the value is as expected.
+     * 
+     * @param dataset
+     *            dataset
+     * @param tag
+     *            tag to check for
+     * @param value
+     *            value of tag to check for
+     */
+    public static void assertValueForTag(Attributes dataset, int tag, String value) {
+
+        String tagKeyword = ElementDictionary.keywordOf(tag, null);
+        Assert.assertTrue("Dataset should contain tag " + tagKeyword, dataset.contains(tag));
+        Assert.assertEquals("Value of tag " + tagKeyword + " should be equal", value, dataset.getString(tag));
+    }
+
+    /**
+     * Check that dataset contains the given tag and the value is not
+     * empty/null.
+     * 
+     * @param dataset
+     *            dataset
+     * @param tag
+     *            tag to check for
+     */
+    public static void assertValueContained(Attributes dataset, int tag) {
+
+        String tagKeyword = ElementDictionary.keywordOf(tag, null);
+        Assert.assertTrue("Dataset should contain a value for tag " + tagKeyword, dataset.containsValue(tag));
+    }
+
+    /**
+     * Check that the given dataset contains all tag values given by a reference
+     * dataset.
+     * 
+     * @param dataset
+     *            dataset to check
+     * @param referenceDataset
+     *            reference dataset
+     */
+    public static void assertContains(Attributes dataset, Attributes referenceDataset)
+    {
+        boolean contains = DicomUtils.contains(dataset, referenceDataset);
+
+        if (!contains)
+        {
+            // TODO output information about failure
+
+            Assert.fail("The dataset does not contain all tags of the test dataset or the values differ.");
+        }
+    }
+
+    /**
+     * Check that ALL of the given datasets contain all of the tag values given
+     * by a reference dataset.
+     * 
+     * @param datasets
+     *            datasets to check
+     * @param referenceDataset
+     *            reference dataset
+     */
+    public static void assertAllContain(List<Attributes> datasets, Attributes referenceDataset)
+    {
+        for (Attributes dataset : datasets)
+        {
+            assertContains(dataset, referenceDataset);
+        }
+    }
+
+    /**
+     * Check that EXACTLY ONE of the given datasets contains all of the tag
+     * values given by a reference dataset.
+     * 
+     * This is useful for checking if a sequence contains a specific item.
+     * 
+     * @param datasets
+     *            datasets to check
+     * @param referenceDataset
+     *            reference dataset
+     * @return the one dataset that contained the reference tag values
+     */
+    public static Attributes assertOneContains(List<Attributes> datasets, Attributes referenceDataset)
+    {
+        List<Attributes> matches = DicomUtils.selectAttributesThatContain(datasets, referenceDataset);
+
+        Assert.assertEquals("Expecting exactly one dataset matching " + referenceDataset.toString(), 1, matches.size());
+
+        return matches.get(0);
+    }
+
+}
---
+++ dcm4che-test/src/main/java/org/dcm4che/test/data/DicomUtils.java
@@ -0,0 +1,139 @@
+//
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Agfa Healthcare.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+package org.dcm4che.test.data;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.io.DicomInputStream;
+
+/**
+ * Helper methods for working with DICOM objects in tests.
+ * 
+ * @author Hermann Czedik-Eysenberg <hermann-agfa@czedik.net>
+ */
+public class DicomUtils {
+
+    /**
+     * Read DICOM object from a file including all bulk data.
+     * 
+     * @param dicomFile
+     * @return attributes
+     * @throws IOException
+     */
+    public static Attributes read(Path dicomFile) throws IOException
+    {
+        return read(new FileInputStream(dicomFile.toFile()));
+    }
+
+    /**
+     * Read DICOM object from a byte array including all bulk data.
+     * 
+     * @param binaryDicomObject
+     * @return attributes
+     * @throws IOException
+     */
+    public static Attributes read(byte[] binaryDicomObject) throws IOException
+    {
+        ByteArrayInputStream binaryDicomObjectStream = new ByteArrayInputStream(binaryDicomObject);
+        return read(binaryDicomObjectStream);
+    }
+
+    /**
+     * Read DICOM object from input stream including all bulk data.
+     * 
+     * @param binaryDicomObjectStream
+     * @return attributes
+     * @throws IOException
+     */
+    public static Attributes read( InputStream binaryDicomObjectStream ) throws IOException
+    {
+        try(DicomInputStream dicomIn = new DicomInputStream( binaryDicomObjectStream ))
+        {
+            return dicomIn.readDataset(-1, -1);
+        }
+    }
+
+    /**
+     * Check that the given dataset contains all tags given by a reference
+     * dataset and also their values are equal.
+     * 
+     * @param dataset
+     *            dataset to check
+     * @param referenceDataset
+     *            reference dataset
+     * @return true if all tags contained and values equal, false otherwise
+     */
+    public static boolean contains(Attributes dataset, Attributes referenceDataset) {
+
+        Attributes selection = new Attributes(dataset, false, referenceDataset);
+
+        return selection.equals(referenceDataset);
+    }
+
+    /**
+     * Select datasets from a list (or Sequence) of datasets that contain all
+     * tag values given by a reference dataset.
+     * 
+     * @param candidateDatasets
+     *            list of candidate datasets
+     * @param referenceDataset
+     *            reference dataset
+     * @return all datasets that contain all of the tag values of the reference
+     *         dataset
+     */
+    public static List<Attributes> selectAttributesThatContain(List<Attributes> candidateDatasets, Attributes referenceDataset)
+    {
+        List<Attributes> matches = new ArrayList<>();
+        for (Attributes candidate : candidateDatasets)
+        {
+            if (contains(candidate, referenceDataset))
+                matches.add(candidate);
+        }
+        return matches;
+    }
+
+}
