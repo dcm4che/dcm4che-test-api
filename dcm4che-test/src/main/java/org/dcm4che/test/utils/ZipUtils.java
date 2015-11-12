2061bc95e03b3b47bc80a5da31596e8ba0b36854
Hermann Czedik-Eysenberg <hermann.czedik-eysenberg@agfa.com>
2015-11-12 11:44:30 +0100

    feat: Add method to extract ZIP file to directory.

---
+++ dcm4che-test/src/main/java/org/dcm4che/test/utils/ZipUtils.java
@@ -0,0 +1,49 @@
+package org.dcm4che.test.utils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.dcm4che3.util.StreamUtils;
+
+/**
+ * Utility methods for working with ZIP files.
+ */
+public class ZipUtils {
+
+    /**
+     * Extracts a zip file to the given directory.
+     *
+     * @param zipFile       path to zip file
+     * @param destDirectory path to directory, must already exist
+     *
+     * @throws IOException IO exception
+     */
+    public void unzip(Path zipFile, Path destDirectory) throws IOException {
+        if (!Files.isDirectory(destDirectory))
+            throw new IOException(destDirectory + " is not a directory");
+
+        try (ZipInputStream zipIn = new ZipInputStream(new BufferedInputStream(Files.newInputStream(zipFile)))) {
+            for (ZipEntry entry = zipIn.getNextEntry(); entry != null; entry = zipIn.getNextEntry()) {
+                Path entryPath = destDirectory.resolve(entry.getName());
+                if (!entry.isDirectory()) {
+                    extractFile(zipIn, entryPath);
+                } else {
+                    Files.createDirectories(entryPath);
+                }
+                zipIn.closeEntry();
+            }
+        }
+    }
+
+    private void extractFile(ZipInputStream zipIn, Path dstFilePath) throws IOException {
+        try (OutputStream out = new BufferedOutputStream(Files.newOutputStream(dstFilePath))) {
+            StreamUtils.copy(zipIn, out);
+        }
+    }
+}
